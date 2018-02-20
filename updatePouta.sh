816aac110638cb13880076efcaecbc19a588f152
Petri Leskinen <petri.leskinen@aalto.fi>
2018-02-20 13:12:25 +0200

    added comment to updatePouta.sh

--- updatePouta.sh
+++ updatePouta.sh
@@ -3,3 +3,4 @@ cat ttl/places/*.ttl > congress_ttl/places.ttl
 scp congress_ttl/*.ttl 193.166.25.181:congress_ttl
 scp update_congress_ttl.sh 193.166.25.181:congress_ttl
 ssh 193.166.25.181
+# on the remote server, run command bash congress_ttl/update_congress_ttl.sh
